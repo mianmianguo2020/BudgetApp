6748479dfd108b90582a9eccd0f5307d3a7b3562
sguo321 <sguo321@gatech.edu>
2023-04-10 23:53:52 -0400

    push budgetapp v1.0 fix style

--- src/component/AddExpenseForm.jsx
+++ src/component/AddExpenseForm.jsx
@@ -1,68 +1,68 @@
-import React, { useContext, useState } from 'react';
-import { AppContext } from '../context/AppContext';
-import { v4 as uuidv4 } from 'uuid';
-import { Button } from 'react-bootstrap';
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
+import { v4 as uuidv4 } from "uuid";
+import { Button } from "react-bootstrap";
 
 const AddExpenseForm = (props) => {
-	const { dispatch } = useContext(AppContext);
+  const { dispatch } = useContext(AppContext);
 
-	const [name, setName] = useState('');
-	const [cost, setCost] = useState('');
+  const [name, setName] = useState("");
+  const [cost, setCost] = useState("");
 
-	const onSubmit = (event) => {
-		event.preventDefault();
-		const expense = {
-			id: uuidv4(),
-			name,
-			cost: parseInt(cost),
-		};
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const expense = {
+      id: uuidv4(),
+      name,
+      cost: parseInt(cost),
+    };
 
-		dispatch({
-			type: 'ADD_EXPENSE',
-			payload: expense,
-		});
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: expense,
+    });
 
-		console.log(expense);
+    console.log(expense);
 
-		setName('');
-		setCost('');
-	};
+    setName("");
+    setCost("");
+  };
 
-	return (
-		<form onSubmit={onSubmit}>
-			<div className='row'>
-				<div className='col-sm col-lg-4'>
-					<label htmlFor='name'>Name</label>
-					<input
-						required='required'
-						type='text'
-						className='form-control'
-						id='name'
-						value={name}
-						onChange={(event) => setName(event.target.value)}
-					/>
-				</div>
-				<div className='col-sm col-lg-4'>
-					<label htmlFor='cost'>Cost</label>
-					<input
-						required='required'
-						type='number'
-						className='form-control'
-						id='cost'
-						value={cost}
-						onChange={(event) => setCost(event.target.value)}
-					/>
-				</div>
-			</div>
-			<div className='row mt-3'>
-				<div className='col-sm'>
-					<button type='submit' className='btn btn-primary'>
-						Save
-					</button>
-				</div>
-			</div>
-		</form>
-	);
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="row">
+        <div className="col-sm col-lg-4">
+          <label htmlFor="name">Name</label>
+          <input
+            required="required"
+            type="text"
+            className="form-control"
+            id="name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div className="col-sm col-lg-4">
+          <label htmlFor="cost">Cost</label>
+          <input
+            required="required"
+            type="number"
+            className="form-control"
+            id="cost"
+            value={cost}
+            onChange={(event) => setCost(event.target.value)}
+          />
+        </div>
+      </div>
+      <div className="row mt-3">
+        <div className="col-sm">
+          <button type="submit" className="btn btn-primary">
+            Save
+          </button>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default AddExpenseForm;
